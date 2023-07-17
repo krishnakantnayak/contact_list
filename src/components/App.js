7c0340428ae0b1eeae229a571e36251a65ab3249
KRISHNA NAYAK <krishnakantn76@gmail.com>
2023-07-18 03:53:08 +0530

    finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,123 @@
-import {getContactList} from "../Controllers/contact_list_fetch";
+
 import Contact from './Contact';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
 
-  const [clist,updateclist]=useState(getContactList());
+  let [clist, setclist] = useState([]);
 
-  // updateclist();
+  let[newcontactname,setnewcontactname]=useState('');
+  let[newcontactnum,setnewcontactnum]=useState('');
+
+  function inputhandler(e){
+    
+    if(e.target.name==='name'){
+      newcontactname=e.target.value;
+      setnewcontactname(newcontactname);
+    }
+    else{
+      newcontactnum=e.target.value;
+      setnewcontactnum(newcontactnum);
+    }
+    
+  }
+
+  async function deletecon(e){
+    console.log(e.target.name);
+
+    let res=await fetch(`https://jsonplaceholder.typicode.com/posts/${e.target.name}`, {
+      method: 'DELETE',
+    });
+
+    if(res.status===200){
+      let nl=[];
+      for(let i of clist){
+        if(i.phone===e.target.name)continue;
+        nl.push(i);
+      }
+  
+      setclist(nl);
+    }
+  }
+
+  async function addnc(){
+    let nc={name:newcontactname, phone:newcontactnum};
+    
+    let res=await fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      body: JSON.stringify({
+        title: 'add_new_con',
+        body: nc,
+        userId: 1,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+
+    if(res.status===201){
+      clist.push(nc);
+      let nl=[...clist];
+      setclist(nl);
+    }
+    
+    
+  }
+
+  async function update(e){
+    console.log(e.target.name);
+    console.log(newcontactname,newcontactnum);
+
+    let nl=[];
+
+    for( let i of clist){
+      if(i.phone===e.target.name){
+        if(newcontactname){
+          i.name=newcontactname;
+        }
+        if(newcontactnum){
+          i.phone=newcontactnum;
+        }
+      }
+
+      nl.push(i);
+      setclist(nl);
+
+      setnewcontactname('');
+      setnewcontactnum('');
+    }
+  }
+
+  useEffect(() => {
+    async function getContactList() {
+
+      const res = await fetch('https://jsonplaceholder.typicode.com/users');
+      const data = await res.json();
+      if(data.length>0)
+        setclist(data);
+    }
+    
+    getContactList();
+    console.log("data", clist)
+
+  }, []);
   return (
     <div className="App">
-      {console.log(clist)}
-      {clist.map((c)=>{return(
-        <Contact name={c.name} image={c.image} number={c.phone}></Contact>
-      );})}
+
+      <div className='add-box'>
+        <input name='name' id='name' type='text' onChange={inputhandler} placeholder='name'></input>
+        <input name='phone' id='phone' type='text' onChange={inputhandler} placeholder='number'></input>
+        <button onClick={addnc}>+ New Contact</button>
+      </div>
+
+      <span className='contact_list'>
+        {clist.map(function (c) {
+          return <Contact name={c.name} number={c.phone} delete={deletecon} update={update} texthandler={inputhandler}></Contact>
+        })}
+      </span>
+
+      
+
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,41 @@
+import { useState } from 'react';
+import def_pp from './images/download.png';
+
 function Contact(props){
+
+    let[editable,seteditable]=useState(false);
+    
+
+    function edit(e){
+        seteditable(true);
+    }
+    function update(e){
+        props.update(e);
+        seteditable(false);
+    }
+
+
     return(
         <div className="contact">
-            <img src="" alt="profile pic"></img>
-            <div className="name">{props.name}</div>
-            <div className="number">{props.number}</div>
+            
+            <img src={def_pp} alt="profile pic"></img>
+            <div className='input-boxes'>
+                <div className={(editable?'forcehide ':'') + "name"}>{props.name}</div>
+                <div className={(editable?'forcehide ':'') + "number"}>{props.number}</div>
+            </div>
+            
+
+            <div className='input-boxes'>
+                <input className={(editable?'':'forcehide ')} name='name' defaultValue={props.name} type='text' onChange={props.texthandler}></input>
+                <input className={(editable?'':'forcehide ')} name='phone' defaultValue={props.number} type='text' onChange={props.texthandler}></input>
+            </div>
+
+            <div className='ctrl-btns'>
+                <button className={(editable?'forcehide ':'') + "edit"} name={props.number} onClick={edit}>Edit</button>
+                <button className={(editable?'':'forcehide ') + "update"} name={props.number} onClick={update}>update</button>
+                <button className='delete' name={props.number} onClick={props.delete}>Delete</button>
+            </div>
+            
         </div>
     );
 }
